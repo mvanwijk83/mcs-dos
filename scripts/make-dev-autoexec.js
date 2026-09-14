644b2eeb58b0a646ab13880a38c29bbd63e7945a
Marcel van Wijk <marcelvwijk@gmail.com>
2026-09-14 02:23:10 +0200

    Cleaning up repo

--- scripts/make-dev-autoexec.js
+++
@@ -1,7 +0,0 @@
-// Personal preferences run in development; releases include an inactive sample.
-const fs=require('fs'),assert=require('assert/strict');
-const petscii=require('./petscii');
-const source=fs.readFileSync('dev/AUTOEXEC.BAT.txt','utf8');
-assert(/^[\r\n\x20-\x7e]*$/.test(source),'Development AUTOEXEC must be printable ASCII or newlines');
-const name=process.argv.includes('--release')?'AUTOEXEC.SAMPLE':'AUTOEXEC.BAT';
-fs.writeFileSync('build/'+name,petscii(source));
